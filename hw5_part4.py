9f913cdd8b1aa8137f761982a0184f0603ebf7e7
RI45816 <uu3@umbc.edu>
2016-10-08 05:21:59 -0400

    ok

--- hw5_part4.py
+++ hw5_part4.py
@@ -1,9 +1,19 @@
-#!/usr/bin/env python3
-#encoding: windows-1252
+# File: hw5_part4.py
+# Author: Uzoma Uwanamodo
+# Date: 10/8/2016
+# Section: 05
+# E-mail: uu3@umbc.edu
+# Description:
+# Determine how many times a string occurs in a larger string
+# Collaboration:
+# I did not collaborate with anyone on this assignment
 
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-if __name__ == "__main__":
-    print("Hello World")
+def main():
+    mainString = input("First (longer) string: ").lower() # Ask user for the main string
+    subString = input("Second (shorter) string: ") # Ask user for the substring
+    checkStrings = [mainString[i:i+len(subString)] for i in range(len(mainString))] # Get the substrings from the main strings that will be matched against the users' substring
+    for i in range(len(checkStrings)):
+        if checkStrings[i] == subString.lower():
+            print("At index",i,"found a slice of", subString)
+            
+main()
